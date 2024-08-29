cf9381aaff6a7a453dc19eeb2e1fb1360549e0b9
renatocrachmad <162373494+renatocrachmad@users.noreply.github.com>
2024-08-29 16:36:19 -0300

    criando projeto

---
+++ bets69/src/main/java/br/com/liberdade/bets69/Bets69Application.java
@@ -0,0 +1,13 @@
+package br.com.liberdade.bets69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bets69Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bets69Application.class, args);
+	}
+
+}
---
+++ bets69/src/test/java/br/com/liberdade/bets69/Bets69ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.liberdade.bets69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bets69ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
